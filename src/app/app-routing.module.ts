d9130e9c7a890ff226fa5410d5136bef082fbd10
ashitosh <ashitoshjadhavj@gmail.com>
2023-01-04 16:39:34 +0530

    medicine-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { MedicinesHomeComponent } from './components/medicines-home/medicines-home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
    {path:'home',component:HomeComponent},
+   {path:'medicines',component:MedicinesHomeComponent},
    {path:'',redirectTo:'/home',pathMatch:'full'},
  {path:'**',component:PageNotFoundComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-
+import { MedicinesHomeComponent } from './components/medicines-home/medicines-home.component';
+import{ BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { TopDealsComponent } from './components/top-deals/top-deals.component';
 @NgModule({
     declarations: [
         AppComponent,
@@ -15,12 +17,17 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
         FooterComponent,
         HomeComponent,
         PageNotFoundComponent,
+        MedicinesHomeComponent,
+        TopDealsComponent,
     ],
     providers: [],
     bootstrap: [AppComponent],
+    
     imports: [
         BrowserModule,
         AppRoutingModule,
+        BrowserAnimationsModule,
+        CarouselModule
     ]
 })
 export class AppModule { }
